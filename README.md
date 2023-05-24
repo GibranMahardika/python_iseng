5b757b8d01c0c395bdcf790343a7f9dbaf18f2f3
GibranMahardika <gibranmahardika96@gmail.com>
2023-05-24 23:51:27 +0700

    1. Added README.md 2. change code mac_changer.py

---
+++ README.md
@@ -0,0 +1,8 @@
+how to use GIT
+
+first use this code at terminal
+git config user.name "GibranMahardika"
+
+git config user.email "gibranmahardika96@gmail.com" #this is for private repository
+
+git config --global user.email "gibranmahardika96@gmail.com" #this is for global repository
--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,19 @@
 import subprocess 
 import optparse
+import re
 
 def GetArguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        # code to handle error
+        parser.error("[-] Please specify an interface, use --help for some more info.")
+    elif not options.new_mac:
+        # code to handle error
+        parser.error("[-] Please specify a mac, use --help for some more info.")
+    return options
     # NOTES = = = = = = = =
     # the option variable is going to contain values of wlan0 and 00:11:22:33:44:55
     # the option variable is contains the values of the user inputs
@@ -25,9 +33,24 @@ def ChangeMac(interface, new_mac):
 # interface = options.interface
 # new_mac = options.new_mac
 
-(options, arguments) = GetArguments()
-ChangeMac(options.interface, options.new_mac)
+def GetCurrentMac(interface):
+    # ChangeMac(options.interface, options.new_mac)
 
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    # options.interface is an argument
+
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        # print(int(mac_address_search_result.group(0)))
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+options = GetArguments()
+
+current_mac = GetCurrentMac(options.interface)
+print("Current MAC = ", current_mac)
 # how to run the code
 # python3 mac_changer.py --interface wlan0 --mac 00:00:00:00:00:11, or
 # python3 mac_changer.py -i wlan0 -m 00:00:00:00:00:11
